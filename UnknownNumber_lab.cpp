3d26656b7232777b951c1dc8876644e9531104df
Уханов Антон <tosha.uhanoff@yandex.ru>
2017-04-26 11:17:03 +0300

    Update UnknownNumber_lab.cpp

--- UnknownNumber_lab.cpp
+++ UnknownNumber_lab.cpp
@@ -117,8 +117,8 @@ struct NewSubj
 			Mark_Lit += itLit->second;
 		}
 		Mark_Lit /= a.Grades.size();
-		int Mark_Lit2 = (int)ceil(Mark_Lit);
-		a.Grades.insert(std::pair<std::string, Grade>("Russian Literature",(Grade) Mark_Lit2));
+		Grade Mark_Lit2 = (Grade)(int)ceil(Mark_Lit);
+		a.Grades.insert(std::pair<std::string, Grade>("Russian Literature", Mark_Lit2));
 	}
 };
 
@@ -162,35 +162,44 @@ struct PrintbyDeutsch
 	}
 };
 
-bool subj_compare(const Student &a, const Student &b)
+struct subj_compare
 {
-	return (a.Grades.size() < b.Grades.size());
-}
-bool math_compare(const Student &a, const Student &b)
-{
-	if (a.Grades.count("math") && b.Grades.count("math"))
+	bool operator()(const Student &a, const Student &b)
 	{
-		return (a.Grades.find("math")->second < b.Grades.find("math")->second);
+		return (a.Grades.size() < b.Grades.size());
 	}
-	else return 0;
-}
-bool mark_compare(const Student &a, const Student &b)
+};
+struct math_compare
 {
-	float Mark_A = 0;
-	float Mark_B = 0;
-	for (auto ita = a.Grades.begin(); ita != a.Grades.end(); ++ita)
+	bool operator()(const Student &a, const Student &b)
 	{
-		Mark_A += ita->second;
+		if (a.Grades.count("math") && b.Grades.count("math"))
+		{
+			return (a.Grades.find("math")->second < b.Grades.find("math")->second);
+		}
+		else return 0;
 	}
-	Mark_A /= a.Grades.size();
+};
+struct mark_compare
+{
+	bool operator()(const Student &a, const Student &b)
+	{
+		float Mark_A = 0;
+		float Mark_B = 0;
+		for (auto ita = a.Grades.begin(); ita != a.Grades.end(); ++ita)
+		{
+			Mark_A += ita->second;
+		}
+		Mark_A /= a.Grades.size();
 
-	for (auto itb = b.Grades.begin(); itb != b.Grades.end(); ++itb)
-	{
-		Mark_B += itb->second;
+		for (auto itb = b.Grades.begin(); itb != b.Grades.end(); ++itb)
+		{
+			Mark_B += itb->second;
+		}
+		Mark_B /= b.Grades.size();
+		return Mark_A < Mark_B;
 	}
-	Mark_B /= b.Grades.size();
-	return Mark_A < Mark_B;
-}
+};
 int main()
 {
 	std::vector<Student> students = LoadStudents();
@@ -210,17 +219,18 @@ int main()
 
 	//insert Litra
 	std::for_each(students.begin(), students.end(), NewSubj());
-	std::cout << "------------------\n";
+
+
 	// max subjects
-	auto it = std::max_element(students.begin(), students.end(), subj_compare);
+	auto it = std::max_element(students.begin(), students.end(), subj_compare());
 	std::cout <<"More subjects : "<< it->FIO <<std::endl;
 	//best matan
 	std::cout << "------------------\n";
-	auto it2 = std::max_element(students.begin(), students.end(), math_compare);
+	auto it2 = std::max_element(students.begin(), students.end(), math_compare());
 	std::cout << "Best math : " << it2->FIO << " ; Mark = " << it2->Grades.find("math")->second << std::endl;
 	//worst mid mark
 	std::cout << "------------------\n";
-	auto it3 = std::min_element(students.begin(), students.end(), mark_compare);
+	auto it3 = std::min_element(students.begin(), students.end(), mark_compare());
 	std::cout << "Worst middle mark : " << it3->FIO  << std::endl;
 	
 	system("pause");
